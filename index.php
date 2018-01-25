ec485c2a3600b465ac2114f215b61af217cb5d11
talha rai <talharaay@gmail.com>
2018-01-25 11:40:28 +0500

    update

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-
     <link href="./behna.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
-<p class="fadeInDown lol">Click the button to display an alert box.</p>
-
-<button class="zoomInUp lol">Try it</button>
+<p class="fadeInDown lol">Welcome to Behna's World</p>
 
 
 </body>
