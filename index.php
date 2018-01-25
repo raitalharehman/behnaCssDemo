9a0e76275aed286fa82f734553831d8a35c53f42
talha rai <talharaay@gmail.com>
2018-01-25 11:35:58 +0500

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link href="http://html.s93.upress.link/mehalev/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<p class="fadeInDown lol">Click the button to display an alert box.</p>
+
+<button class="zoomInUp lol">Try it</button>
+
+
+</body>
+</html>
