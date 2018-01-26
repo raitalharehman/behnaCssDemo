eaabd5337df77412e594e9034d4b42249a8dba05
talha rai <talharaay@gmail.com>
2018-01-26 18:09:09 +0500

    inspect disable

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="icon" href="giphy.gif" type="image/gif" sizes="16x16">
     <title>Behna</title>
     <link href="./behna.css" rel="stylesheet" type="text/css">
+    <script src="jquery-1.12.3.min.js"></script>
     <style>
         body {
             height: 100vh;
@@ -16,10 +17,49 @@
 
         h1 {
             margin-top: 40vh;
+            cursor: wait;
+        }
+        h5 {
+            cursor: none;
         }
     </style>
+    <script>
+        // Keys Disable for inspect
+        $(document).keydown(function (event) {
+            // if (event.keyCode === 123) {
+            //     return false;
+            // }
+            // else if (event.keyCode === 116) {
+            //     return false;
+            // }
+            // else if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
+            //     return false;
+            // }
+            // else if (event.ctrlKey && event.shiftKey && event.keyCode === 105) {
+            //     return false;
+            // }
+            // else if (event.ctrlKey && event.keyCode === 85) {
+            //     return false;
+            // }
+            // else if (event.ctrlKey && event.keyCode === 117) {
+            //     return false;
+            // }
+            // else if (event.ctrlKey && event.keyCode === 82) {
+            //     return false;
+            // }
+            // else if (event.ctrlKey && event.keyCode === 114) {
+            //     return false;
+            // }
+                return false;
+        });
+        // Right Click Disable
+        $(document).on("contextmenu", function (e) {
+            e.preventDefault();
+        });
+    </script>
 </head>
 <body>
 <h1 class="fadeInDown lol">Welcome to Behna's World</h1>
+<h5>Try to Inspect me</h5>
 </body>
 </html>
