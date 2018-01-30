f3ea385915ec92984675f6cc92bca2efe86c1c43
talha rai <talharaay@gmail.com>
2018-01-30 16:47:27 +0500

    animations with keys

--- index.php
+++ index.php
@@ -6,38 +6,13 @@
     <link rel="icon" href="giphy.gif" type="image/gif" sizes="16x16">
     <title>Behna</title>
     <link href="./behna.css" rel="stylesheet" type="text/css">
+    <link href="./style.css" rel="stylesheet" type="text/css">
     <script src="jquery-1.12.3.min.js"></script>
-    <style>
-        body {
-            height: 100vh;
-            width: 100%;
-            text-align: center;
-            overflow: hidden;
-        }
-        html {
-            cursor: wait;
-        }
-
-        h1 {
-            margin-top: 40vh;
-        }
-        h5 {
-            cursor: none;
-        }
-    </style>
-    <script>
-        // Keys Disable for inspect
-        $(document).keydown(function (event) {
-                return false;
-        });
-        // Right Click Disable
-        $(document).on("contextmenu", function (e) {
-            e.preventDefault();
-        });
-    </script>
 </head>
 <body>
 <h1 class="fadeInDown lol">Welcome to Behna's World</h1>
 <h5>Try to Inspect me</h5>
+
+<script src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,68 @@
+// Keys Disable for inspect
+$(document).keydown(function (event) {
+    if (event.keyCode === 123) {
+        return false;
+    }
+    else if (event.keyCode === 116) {
+        return false;
+    }
+    else if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
+        return false;
+    }
+    else if (event.ctrlKey && event.shiftKey && event.keyCode === 105) {
+        return false;
+    }
+    else if (event.ctrlKey && event.keyCode === 85) {
+        return false;
+    }
+    else if (event.ctrlKey && event.keyCode === 117) {
+        return false;
+    }
+    else if (event.ctrlKey && event.keyCode === 82) {
+        return false;
+    }
+    else if (event.ctrlKey && event.keyCode === 114) {
+        return false;
+    }
+});
+// Right Click Disable
+$(document).on("contextmenu", function (e) {
+    e.preventDefault();
+});
+var num = 0;
+var animate = ["", "bounce", "flash", "pulse",
+    "rubberBand", "shake", "swing", "tada", "wobble", "jello", "bounceIn", "bounceInDown", "bounceInLeft",
+    "bounceInRight", "bounceInUp", "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp",
+    "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig", "fadeInRight", "fadeInRightBig",
+    "fadeInUp", "fadeInUpBig", "fadeOut", "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig",
+    "fadeOutRight", "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig", "flip", "flipInX", "flipInY", "flipOutX",
+    "flipOutY", "lightSpeedIn", "lightSpeedOut", "rotateIn", "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft",
+    "rotateInUpRight", "rotateOut", "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", "rotateOutUpRight",
+    "slideInUp", "slideInDown", "slideInLeft", "slideInRight", "slideOutUp", "slideOutDown", "slideOutLeft",
+    "slideOutRight", "zoomIn", "zoomInDown", "zoomInLeft", "zoomInRight", "zoomInUp", "zoomOut", "zoomOutDown",
+    "zoomOutLeft", "zoomOutRight", "zoomOutUp", "hinge", "jackInTheBox", "rollIn", "rollOut"];
+
+function animation(animate) {
+
+    var target = $("html");
+
+    $(window).keydown(function (e) {
+        if (e.keyCode === 0 || e.keyCode === 39 || e.keyCode === 38) {
+            e.preventDefault();
+            target.removeClass(animate[num]);
+            target.addClass(animate[++num] + ' lol');
+        }
+        else if (e.keyCode === 0 || e.keyCode === 37 || e.keyCode === 40) {
+            e.preventDefault();
+            if (num === 0) {
+                return false
+            }
+            else {
+                target.removeClass(animate[num]);
+                target.addClass(animate[--num] + ' lol');
+            }
+        }
+    })
+}
+
+animation(animate);
